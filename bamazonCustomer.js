fbee636c2354f0e68519c026567496049f637cb3
Ethan Favia <efavia215@live.com>
2016-07-29 12:57:19 -0400

    working response from server and display

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,34 @@
-const 
+const inquirer = require('inquirer');
+
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    user: 'root',
+    password: '',
+    host: 'localhost',
+    port: 3306,
+    database: 'Bamazon'
+});
+
+connection.connect(function(err){
+    if(err){
+        throw err;
+    } 
+    console.log('Successfully connected to your DB\n');
+    displayItems();
+});
+
+function displayItems () {
+    connection.query('SELECT * FROM Products', function(err, res){
+        if (err){throw err}
+
+        res.forEach(function(row) {
+        console.log('Product ID: ' + row.ItemID);
+        console.log('Product Name: ' + row.ProductName);
+        console.log('DepartmentName: ' + row.DepartmentName);
+        console.log('Price: ' + row.Price);
+        console.log('Stock Quantity: ' + row.StockQuantity);
+        console.log('-------------------------------')
+        })
+    });
+};
